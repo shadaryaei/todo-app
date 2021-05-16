745d4cc82980715a1b7d56988714f0d650074d92
hamid rahmani <ha.rahmani@pinket.com>
2021-05-16 15:56:14 +0430

    update serviceworker

--- src/index.js
+++ src/index.js
@@ -9,4 +9,21 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register({
+    onUpdate: (registration) => {
+        const waitingServiceWorker = registration.waiting
+
+        if (waitingServiceWorker) {
+            alert('you need to reload the page')
+            waitingServiceWorker.addEventListener("statechange", (event) => {
+              console.log(event)
+              // if (event.target.state === "activated") {
+              //   window.location.reload()
+              // }
+            });
+      
+            waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+          }
+
+    }
+})
