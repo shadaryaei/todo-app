3c93d0dd7f81af926d09e42550eec93aa7ba6ecf
hamid rahmani <ha.rahmani@pinket.com>
2021-05-16 15:57:43 +0430

    fix indention

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({
-    onUpdate: (registration) => {
-        const waitingServiceWorker = registration.waiting
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
 
-        if (waitingServiceWorker) {
-            alert('you need to reload the page')
-            waitingServiceWorker.addEventListener("statechange", (event) => {
-              console.log(event)
-              // if (event.target.state === "activated") {
-              //   window.location.reload()
-              // }
-            });
-      
-            waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-          }
+    if (waitingServiceWorker) {
+      alert("you need to reload the page");
+      waitingServiceWorker.addEventListener("statechange", (event) => {
+        console.log(event);
+        // if (event.target.state === "activated") {
+        //   window.location.reload()
+        // }
+      });
 
+      waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
     }
-})
+  },
+});
