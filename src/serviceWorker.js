61c60f685e2ca4db255a9630ab524fe5cea39f0f
hamid rahmani <ha.rahmani@pinket.com>
2021-05-16 16:24:36 +0430

    update serviceworker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -55,6 +55,7 @@ export function register(config) {
 }
 
 function registerValidSW(swUrl, config) {
+  console.log(config);
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
